3f3d71cd524b4a752da667a60f787e80351e8bf4
BohdanKlymenko <81781697+BohdanKlymenko@users.noreply.github.com>
2021-09-21 21:05:57 +0300

    форматирует код

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,9 +1,10 @@
 const delay = ms => {
-    return new Promise((resolve) => {
-        setTimeout(() => { resolve(ms) }, ms)
-    })
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  });
 };
-
 
 const logger = time => console.log(`Resolved after ${time}ms`);
 
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,13 +6,13 @@ const users = [
 ];
 
 const toggleUserState = (allUsers, userName) => {
-    
   const updatedUsers = allUsers.map(user =>
     user.name === userName ? { ...user, active: !user.active } : user,
   );
-    
-    return new Promise((resolve) => {resolve(updatedUsers)
-    })
+
+  return new Promise(resolve => {
+    resolve(updatedUsers);
+  });
 };
 
 const logger = updatedUsers => console.table(updatedUsers);
